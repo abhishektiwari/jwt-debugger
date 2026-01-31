4e2823e56c2b7e38844da4214939144e82ac6621
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2026-01-31 16:01:00 -0500

    Adding Github to menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,6 +5,17 @@
         <q-toolbar-title>
           JWT Debugger
         </q-toolbar-title>
+
+        <q-btn
+          flat
+          dense
+          icon="open_in_new"
+          label="GitHub"
+          @click="openGitHub"
+          no-caps
+        >
+          <q-tooltip>View on GitHub</q-tooltip>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
@@ -15,12 +26,20 @@
 </template>
 
 <script>
+import { openURL } from 'quasar'
+
 export default {
   name: 'MainLayout',
   components: {
   },
   data () {
     return {
+      githubUrl: 'https://github.com/abhishektiwari/jwt-debugger'
+    }
+  },
+  methods: {
+    openGitHub() {
+      openURL(this.githubUrl)
     }
   }
 }
